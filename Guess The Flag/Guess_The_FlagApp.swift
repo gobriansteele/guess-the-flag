0c8785b9d242796ae01b08c947d5b83a6a83da04
Brian Steele <brian.steele@hey.com>
2021-12-19 14:34:38 -0800

    Initial Commit

---
+++ Guess The Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess The Flag
+//
+//  Created by Brian Steele on 12/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
